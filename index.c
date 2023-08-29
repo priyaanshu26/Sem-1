bb99d0b21ab36938faf10819d7e1b8f9c8d24d95
Priyanshu Choudhary <136172891+priyaanshu26@users.noreply.github.com>
2023-08-30 01:23:47 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    int a,b,c;
+    printf("Enter a integer value:");
+    scanf ("%d", &a);
+    printf("Enter b integer value:");
+    scanf ("%d", &b);
+    c=a+b;
+    printf ("Sum is:%d" , c);
+}
---
+++ oddbitwise.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main() 
+{
+    int n;
+    printf("Enter value of n:");
+    scanf("%d",&n);
+    (n%2==0)?(printf("n is even")):(printf("n is odd"));
+}
